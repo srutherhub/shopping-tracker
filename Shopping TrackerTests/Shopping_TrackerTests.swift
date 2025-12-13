fc31cecb0ea6b749a1efbe7904747a8d05d2dbd6
srutherhub <samuelbrutherford@gmail.com>
2025-12-13 10:11:12 -0600

    Initial Commit

---
+++ Shopping TrackerTests/Shopping_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_TrackerTests.swift
+//  Shopping TrackerTests
+//
+//  Created by Samuel Rutherford on 12/13/25.
+//
+
+import Testing
+@testable import Shopping_Tracker
+
+struct Shopping_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Shopping TrackerUITests/Shopping_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Shopping_TrackerUITestsLaunchTests.swift
+//  Shopping TrackerUITests
+//
+//  Created by Samuel Rutherford on 12/13/25.
+//
+
+import XCTest
+
+final class Shopping_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
